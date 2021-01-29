027a53416edc217bb91231e2bb09a029af14e26c
UW_Drama <prcomm2drama@gmail.com>
2021-01-29 10:57:06 -0800

    added content in twelfth-night-production.json

--- js/twelfth-night-page.js
+++ js/twelfth-night-page.js
@@ -4,26 +4,41 @@
 let rowGenerator = 0;
 
 
-function createRow(state){
+function createCastRow(state){
   $("#js-cast-table").append("<tr id='js-row" + rowGenerator + "'></tr>")
-  $("#js-row" + rowGenerator).append("<td class='py-2'>" + state.name + "</td>")
-  $("#js-row" + rowGenerator).append("<td class='py-2'>" + state.role + "</td>")
-  $("#js-row" + rowGenerator).append("<td class='py-2'>" + state.school + "</td>")
+  $("#js-row" + rowGenerator).append("<td class='p-2'>" + state.name + "</td>")
+  $("#js-row" + rowGenerator).append("<td class='p-2'>" + state.role + "</td>")
+  $("#js-row" + rowGenerator).append("<td class='p-2'>" + state.school + "</td>")
+
+  rowGenerator++;
+}
+
+function createProductionRow(state){
+  $("#js-production-table").append("<tr id='js-row" + rowGenerator + "'></tr>")
+  $("#js-row" + rowGenerator).append("<td class='p-2'>" + state.name + "</td>")
+  $("#js-row" + rowGenerator).append("<td class='p-2'>" + state.role + "</td>")
+  $("#js-row" + rowGenerator).append("<td class='p-2'>" + state.school + "</td>")
 
   rowGenerator++;
 }
 
 
-function renderRows(arrayObjects){
-  for(let i = 0; i < arrayObjects.length; i++){
-    createRow(arrayObjects[i]);
+function renderRows(arrayObjects, name){
+  if(name.includes("cast")){
+    for(let i = 0; i < arrayObjects.length; i++){
+      createCastRow(arrayObjects[i]);
+    }
+  }else if(name.includes("production")){
+    for(let i = 0; i < arrayObjects.length; i++){
+      createProductionRow(arrayObjects[i]);
+    }
   }
-
 }
 let castsArray =[];
-function fetchEvents(){
+
+function fetchEvents(jsonPath){
   let promise;
-  fetch(new Request('./json/twelfth-night-cast.json'))
+  fetch(new Request(jsonPath))
   .then(function(response){
     promise = response.json();
     return promise;
@@ -31,7 +46,7 @@ function fetchEvents(){
   .then(data => {
     castsArray = data.result;
     console.log(data.result);
-    renderRows(data.result);
+    renderRows(data.result, jsonPath);
 
   }).catch(function(err){
     renderError(err);
@@ -40,7 +55,9 @@ function fetchEvents(){
   return promise;
 }
 
-fetchEvents();
+fetchEvents('./json/twelfth-night-cast.json');
+fetchEvents('./json/twelfth-night-production.json');
+
 
 function renderError(err){
   $("#js-flex-pages").append("<p class='alert alert-danger'>" +err.message + "</p>");
